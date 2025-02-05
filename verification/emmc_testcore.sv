1376429a27039e17294ad06effeb58bd225196ab
Roman Kordyukov <romakeii@yandex.ru>
2025-02-05 16:15:41 +0300

    emmc_testcore module was changed: checkerboard algorithm was implemented

--- verification/emmc_testcore.sv
+++ verification/emmc_testcore.sv
@@ -34,19 +34,29 @@ module emmc_testcore #(
 
 	logic host_we;
 	logic host_start;
-	logic [7 : 0] host_wr_dat;
+	(* DONT_TOUCH *)logic [7 : 0] host_wr_dat;
 	logic [7 : 0] host_rd_dat;
 	logic host_dvalid;
 	logic host_ready;
 
 	assign host_start = 1;
 
-	localparam logic [1 : 0] BLK_CNT = 2;
-	logic [15 : 0] blk_idx;
+	logic host_in_run_mode;
 	always_ff @(posedge clk_core or posedge rst_tk) begin
-		if(rst_tk)          blk_idx <= 0;
-		else if(host_ready) blk_idx <= blk_idx + BLK_CNT;
+		if(rst_tk)          host_in_run_mode <= 0;
+		else if(host_ready) host_in_run_mode <= 1;
 	end
+
+	localparam logic [1 : 0] BLK_CNT = 2;
+	logic [31 : 0] blk_idx;
+	localparam logic [$bits(blk_idx) - 1 : 0] AMOUNT_OF_BLKS = 1562500;
+	logic chb_started_all;
+	always_ff @(posedge clk_core or posedge rst_tk) begin
+		if(rst_tk)                                           blk_idx <= 0;
+		else if(blk_idx < AMOUNT_OF_BLKS && chb_started_all && host_dvalid) blk_idx <= blk_idx + BLK_CNT;
+	end
+	logic chb_enbl;
+	assign chb_enbl = host_in_run_mode & host_dvalid;
 	emmc_sm emmc_sm_inst (
 		.clk_i(clk_core),
 		.arst_i(rst_tk),
@@ -71,20 +81,18 @@ module emmc_testcore #(
 		.ready_o(host_ready)
 	);
 
-	logic host_in_run_mode;
+	(* DONT_TOUCH *)logic is_err;
 	always_ff @(posedge clk_core or posedge rst_tk) begin
-		if(rst_tk)          host_in_run_mode <= 0;
-		else if(host_ready) host_in_run_mode <= 1;
+		if(rst_tk)                                      is_err <= 0;
+		else if(!host_we && host_dvalid && host_wr_dat != host_rd_dat) is_err <= 1;
 	end
-
-	logic chb_enbl;
-	assign chb_enbl = host_in_run_mode & host_dvalid;
 
 	ag_checkerboard #(.LENGTH(512 * BLK_CNT)) ag_checkerboard_inst (
 		.clk_i(clk_core),
 		.srst_i(rst_tk),
 		.enbl_i(chb_enbl),
-		.finished_o(),
+		.started_part_o(),
+		.started_all_o(chb_started_all),
 		.wr_data_o(host_wr_dat),
 		.wr_enbl_o(host_we)
 	);
@@ -99,17 +107,24 @@ module emmc_testcore #(
 			assign ila_blob = {
 				emmc_sm_inst.we_i,
 				emmc_sm_inst.start_i,
-				emmc_sm_inst.dat_i,
-				emmc_sm_inst.dat_o,
 				emmc_sm_inst.dvalid_o,
 				emmc_sm_inst.ready_o,
 				emmc_sm_inst.orig_state,
 				emmc_sm_inst.curr_state,
 				emmc_sm_inst.next_state,
 				emmc_sm_inst.state_change_enbl,
+				emmc_sm_inst.fsm_dat_not_busy,
+				emmc_sm_inst.dath_crc_ok,
 				cmd_io,
 				dat_io,
-				emmc_sm_inst.card_status
+				host_wr_dat,
+				host_rd_dat,
+				is_err,
+				host_we,
+				ag_checkerboard_inst.work_counter,
+				ag_checkerboard_inst.part_counter,
+				blk_idx,
+				chb_started_all
 			};
 			ila ila_inst (
 				.clk(clk_core),
@@ -125,6 +140,12 @@ module emmc_testcore #(
 
 		end else begin
 			assign rst_tk = arst_i;
+
+			initial begin
+
+
+
+			end
 		end
 
 	endgenerate
